0dde2f24415f99930f6dc44003d39026e8179f85
andrewmcgov <andrew.mcgoveran@shopify.com>
2017-10-29 19:20:48 -0400

    added jQuery, set up nav open/close

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 console.log('Welcome to Erin\'s site!');
+
+window.app = window.app || {};
+
+app = (function() {
+  // Log when app is running
+  console.log('App is running');
+
+  // Set up app selectors 
+  this.selectors = {
+    header: '.header',
+    headerRow: '.header__row',
+    mobileNav: '.mobileNav',
+    mobileNavItems: '.mobile-nav__list a',
+    navTrigger: '#mobileNavTrigger'
+  }
+
+  // Nav trigger click 
+  $(selectors.navTrigger).click(function() {
+    $(selectors.header).toggleClass('mobile-nav--open');
+  });
+
+  // Close mobile nav on link click
+  $(selectors.mobileNavItems).each(function() {
+    $(this).click(function() {
+      $(selectors.header).removeClass('mobile-nav--open');
+    });
+  });
+
+
+
+});
+
+$(document).ready(function(){
+  app();
+}); 
